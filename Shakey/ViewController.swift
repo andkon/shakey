5bb1cfc2b7096af1fbeda3ae2a09b2bc2274df3a
Andrew Konoff <andrew.konoff@gmail.com>
2016-01-10 21:43:42 -0800

    Initial Commit

---
+++ Shakey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shakey
+//
+//  Created by Andrew Konoff on 2016-01-10.
+//  Copyright © 2016 Andrew Konoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
